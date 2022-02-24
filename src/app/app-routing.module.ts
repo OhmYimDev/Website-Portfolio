171d2b5586fa3bc4e77ae1d1a5583a088f520d3c
OhmYimDev <phongphan_dw@hotmail.com>
2022-02-24 22:28:35 +0700

    update project details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './component/homepage/homepage.component';
 import { Project1Component } from './component/project1/project1.component';
+import { Project2Component } from './component/project2/project2.component';
+import { Project3Component } from './component/project3/project3.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'project1', component: Project1Component}
+  { path: 'project1', component: Project1Component },
+  { path: 'project2', component: Project2Component },
+  { path: 'project3', component: Project3Component }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomepageComponent } from './component/homepage/homepage.component';
 import { Project1Component } from './component/project1/project1.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { Project2Component } from './component/project2/project2.component';
+import { Project3Component } from './component/project3/project3.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { FooterComponent } from './component/footer/footer.component';
     Project1Component,
     NavbarComponent,
     FooterComponent,
+    Project2Component,
+    Project3Component,
   ],
   imports: [
     BrowserModule,
