0753bd2f1304f87d835919a58856822f6f8e53ea
OhmYimDev <phongphan_dw@hotmail.com>
2022-02-25 18:31:01 +0700

    make it better

--- src/app/server.js
+++ src/app/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/dopefolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/dopefolio'}),
+    res.sendFile({root: 'dist/dopefolio'}, 'index.html'),
 );
 
 // Start the app by listening on the default Heroku port
